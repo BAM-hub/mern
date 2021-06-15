c2cd55a7be47a10ff887afc6c2aeceb871f7a458
BAM-hub <bsharamin12@gmail.com>
2021-06-15 04:57:52 +0300

    Api Routes Added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/auth
+// @route   GET api/posts
 // @desc    Test route
 // @access  public
-router.get('/', (req, res) => res.send('auth route'));
+router.get('/', (req, res) => res.send('posts route'));
 
 module.exports = router;
