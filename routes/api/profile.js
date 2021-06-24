94184705506b7eac390414a3b29b643beb50870e
BAM-hub <bsharamin12@gmail.com>
2021-06-24 04:55:32 +0300

    added the git hub get repos logic and done with the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
@@ -300,5 +302,30 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     }
   }
 );
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github 
+// @access  public
 
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'nodes.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('server error');
+  }
+});
 module.exports = router;
