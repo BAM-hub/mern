799d63345e20624e7c67c0455adbadc0cbfba878
BAM-hub <bsharamin12@gmail.com>
2021-06-26 19:36:22 +0300

    init alret

--- client/src/components/layout/auth/Register.js
+++ client/src/components/layout/auth/Register.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../../actions/alert';
 
-const Register = () => {
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,7 +19,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2)
-      console.log('passwords dont match');
+      props.setAlert('passwords dont match', 'danger');
     else {
      console.log('Success');
     } 
@@ -81,6 +83,6 @@ return (
       </p>
     </Fragment>
   );
-}
+};
 
-export default Register;
+export default connect(null, { setAlert })(Register);
