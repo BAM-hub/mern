2de8ebf1aa7bbc828b83456c99f84a1478cba5c3
BAM-hub <bsharamin12@gmail.com>
2021-06-28 04:10:24 +0300

    the private route componnent

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+
+const PrivateRoute = (
+  {
+    componenet: Componenet,
+    auth: {isAuthenticated, loading},
+    ...rest 
+  }
+) => (
+  <Route 
+    {...rest} 
+    render={props => 
+      !isAuthenticated && !loading ? (
+      <Redirect to='/login' />
+      ) : (
+      <Componenet {...props} />
+      ) 
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
