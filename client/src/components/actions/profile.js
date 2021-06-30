4260dc9a8786f4a08b159e0e70fd3b679084697b
BAM-hub <bsharamin12@gmail.com>
2021-06-30 05:20:16 +0300

    create profile logic

--- client/src/components/actions/profile.js
+++ client/src/components/actions/profile.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 
 import {
   GET_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  SET_ALERT
 } from './types';
 
 //get current users profile
@@ -26,3 +27,40 @@ export const getCurrentProfile = () => async dispatch => {
    });   
   }
 };
+
+
+// create or update a profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(SET_ALERT(edit? 'profile updated' : 'profile created', 'success'));
+
+    if(!edit) {
+      history.push('/dashboard');  //redirects user
+    }    
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+    type: PROFILE_ERROR,
+    payload: { 
+      msg: err.response.statusText,
+      status: err.response.status 
+    }
+    });   
+  }
+}
