2e33e82f067c132637b44eca1bacbf4baa9b00c7
BAM-hub <bsharamin12@gmail.com>
2021-07-02 15:12:42 +0300

    education and expeirence requests handled

--- client/src/components/actions/profile.js
+++ client/src/components/actions/profile.js
@@ -4,7 +4,8 @@ import { setAlert } from './alert';
 import {
   GET_PROFILE,
   PROFILE_ERROR,
-  SET_ALERT
+  SET_ALERT,
+  UPDATE_PROFILE
 } from './types';
 
 //get current users profile
@@ -63,4 +64,75 @@ export const createProfile = (formData, history, edit = false) => async dispatch
     }
     });   
   }
-}
+};
+
+// Add expirence
+
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const res = await axios.put('/api/profile/experience', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(SET_ALERT('Experience added', 'success'));
+
+    history.push('/dashboard');  //redirects user
+       
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+    type: PROFILE_ERROR,
+    payload: { 
+      msg: err.response.statusText,
+      status: err.response.status 
+    }
+    });   
+  }
+};
+
+// Add Education
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const res = await axios.put('/api/profile/education', formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(SET_ALERT('Education added', 'success'));
+
+    history.push('/dashboard');  //redirects user
+       
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+    type: PROFILE_ERROR,
+    payload: { 
+      msg: err.response.statusText,
+      status: err.response.status 
+    }
+    });   
+  }
+};
