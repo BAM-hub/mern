b028cd7af0b6f1a22b1ec2cce0e1adb322d1ef20
BAM-hub <bsharamin12@gmail.com>
2021-07-02 19:38:44 +0300

    the delete logic for experience, edu and profile

--- client/src/components/actions/profile.js
+++ client/src/components/actions/profile.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { setAlert } from './alert';
 
 import {
+  CLEAR_PROFILE,
   GET_PROFILE,
   PROFILE_ERROR,
   SET_ALERT,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  ACCOUNT_DELETED
 } from './types';
 
 //get current users profile
@@ -135,4 +137,68 @@ export const addEducation = (formData, history) => async dispatch => {
     }
     });   
   }
+};
+
+// Delete experience
+export const  deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.profile
+    });
+    dispatch(setAlert('Experience Removed', 'success'));
+  } catch (err) {
+    dispatch({
+    type: PROFILE_ERROR,
+    payload: { 
+      msg: err.response.statusText,
+      status: err.response.status 
+    }
+    }); 
+  }
+};
+
+// Delete education
+export const  deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.profile
+    });
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (err) {
+    dispatch({
+    type: PROFILE_ERROR,
+    payload: { 
+      msg: err.response.statusText,
+      status: err.response.status 
+    }
+    }); 
+  }
+};
+
+// Delete Account & profile
+export const  deleteAccount = () => async dispatch => {
+  if(window.confirm('Are you sure? This can NOT be undone')) {
+    try {
+      const res = await axios.delete('/api/profile');
+  
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+      dispatch(setAlert('Your Account has been deleted'));
+    } catch (err) {
+      dispatch({
+      type: PROFILE_ERROR,
+      payload: { 
+        msg: err.response.statusText,
+        status: err.response.status 
+      }
+      }); 
+    }
+  }
+  
 };
