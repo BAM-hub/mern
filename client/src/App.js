54fbed47a8ae30be14756fe4e7f62015a035cb16
BAM-hub <bsharamin12@gmail.com>
2021-09-29 03:41:57 +0300

    post link is added

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import EditProfile from './components/profile-form/EditProfile';
 import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Posts from './components/posts/Posts';
 import Profile from './components/profile/Profile';
 // redux
 import { Provider } from 'react-redux';
@@ -48,6 +49,7 @@ const App = () => {
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               <PrivateRoute exact path="/add-experience" component={AddExperience} />
               <PrivateRoute exact path="/add-education" component={AddEducation} />
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
