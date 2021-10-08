23c142a3892912e460d299af98fed4b0c3534748
BAM-hub <bsharamin12@gmail.com>
2021-10-08 21:00:07 +0300

    postItem Page functions

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../actions/post';
+import { addLike, removeLike, deletePost } from '../actions/post';
 
 const PostItem = ({
 addLike,
 removeLike, 
+deletePost,
 auth,
 post: {
   _id,
@@ -57,7 +58,8 @@ post: {
               )}
             </Link>
             {!auth.loading && user === auth.user._id && (
-            <button      
+            <button
+            onClick={e => deletePost(_id)}      
             type="button"
             class="btn btn-danger"
             >
@@ -70,7 +72,10 @@ post: {
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -79,5 +84,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps, 
-  { addLike, removeLike }
+  { addLike, removeLike, deletePost }
 )(PostItem);
