f0422b5a4b40724eb4f95dc1e2ff1ecdbc9f50db
BAM-hub <bsharamin12@gmail.com>
2021-10-08 20:04:56 +0300

    implemint add and remove like featuers in oostitem

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../actions/post';
 
-const PostItem = ({ 
+const PostItem = ({
+addLike,
+removeLike, 
 auth,
 post: {
   _id,
@@ -37,13 +40,13 @@ post: {
                   {date}
                 </Moment>
             </p>
-            <button type="button" class="btn btn-light">
+            <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
               <i class="fas fa-thumbs-up"></i> {' '}
               <span>{likes.length > 0 && (
                 <span>{likes.length}</span>
               )}</span>
             </button>
-            <button type="button" class="btn btn-light">
+            <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
               <i class="fas fa-thumbs-down"></i>
             </button>
             <Link to={`/post/${_id}`} class="btn btn-primary">
@@ -75,5 +78,6 @@ const mapStateToProps = state => ({
 });
 
 export default connect(
-  mapStateToProps, {}
+  mapStateToProps, 
+  { addLike, removeLike }
 )(PostItem);
